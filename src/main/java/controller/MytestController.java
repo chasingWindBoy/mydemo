a12063d4e9da9883cc69a483a7f0bdee2c5df6d2
greg <greg@yeahka.com>
2019-10-12 10:48:32 +0800

    我是你大爷

--- src/main/java/controller/MytestController.java
+++ src/main/java/controller/MytestController.java
@@ -2,7 +2,7 @@ package controller;
 
 public class MytestController {
 	public static void main(String[] args) {  
-		System.out.println("Test范德萨发大厦，我是来改动的");
+		System.out.println("Test范德萨发大厦，我是来改动的，首班");
 	}
 
 }
