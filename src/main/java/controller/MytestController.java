dd5ddcfa6f8eb7784c6ae94ae20d5e478400cb58
greg <greg@yeahka.com>
2019-10-12 09:50:55 +0800

    防守打法的

--- src/main/java/controller/MytestController.java
+++ src/main/java/controller/MytestController.java
@@ -1,8 +1,8 @@
 package controller;
 
 public class MytestController {
-	public static void main(String[] args) { 
-		System.out.println("Test");
+	public static void main(String[] args) {  
+		System.out.println("Test·¶µÂÈø·¢´óÏÃ");
 	}
 
 }
